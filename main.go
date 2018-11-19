c2767528943e63768296e98c732c81eaf0548364
riemann89 <giu.giffone@tiscali.it>
2018-11-19 17:15:44 +0000

    add init random state

--- main.go
+++ main.go
@@ -1,24 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 func main() {
 
 	var currentState State
-	var newState State
+	//var newState State
 
-	nState := 10
+	states := [2]bool{true, false}
+	//nState := 10
 	size := 10
+	currentState = make([][]bool, size)
+	for i:= 0; i< size; i++{
+		currentState[i] = make([]bool, size)
+	}
 
 	//init a random state
-	for i := 0; i < nState; i++ {
+	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
-
+			currentState[i][j] = states[rand.Intn(2)]
 		}
 	}
 
-
-	for i := 0; i < nState; i++ {
-		for j := 0; j < size; j++ {
-
-		}
+	for i:= 0; i < size; i++{
+		fmt.Println(currentState[i])
 	}
+
+
+
 }
+/*
+	for i := 0; i < nState; i++ {
+		for j := 0; j < size; j++ {
+
+		}
+	}
+*/
