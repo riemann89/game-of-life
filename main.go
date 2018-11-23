f9a59980e718386f6379ca1852959aa3e07359c6
riemann89 <giu.giffone@tiscali.it>
2018-11-23 00:22:41 +0000

    fix bug stateTransition, added block still life function generation

--- main.go
+++ main.go
@@ -2,39 +2,29 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 )
 
 func main() {
 
 	var currentState State
-	//var newState State
-
-	states := [2]uint8{0, 1}
-	//nState := 10
-	size := 10
-	currentState = make([][]uint8, size)
-	for i:= 0; i< size; i++{
-		currentState[i] = make([]uint8, size)
-	}
-
-	//init a random state
-	//TODO FIX PSEUDORANDOM	
+	//states := [2]string{" ", "*"}
+	size := 4
+	currentState = make([][]string, size)
 	for i := 0; i < size; i++ {
-		for j := 0; j < size; j++ {
-			currentState[i][j] = states[rand.Intn(2)]
-		}
+		currentState[i] = make([]string, size)
 	}
+	
+	//init stable status
+	currentState = BlockState(size)
 
-	for k:=0; k<=2; k++{
-		for i:= 0; i < size; i++{
+	for k := 0; k <= 10; k++ {
+		for i := 0; i < size; i++ {
 			fmt.Println(currentState[i])
 		}
 		fmt.Println("")
 		time.Sleep(time.Second)
 		currentState = currentState.StateTransition(size)
 	}
-
 
 }
--- state.go
+++ state.go
@@ -1,32 +1,34 @@
 package main
 
 //State type of a system
-type State [][]uint8
+type State [][]string
 
 //StateTransition computes the transition between a state into another
 func (currentState State) StateTransition(size int) State {
 
-	newState := State(make([][]uint8, size))
+	newState := State(make([][]string, size))
 	
 	for i:=0; i< size; i++{
-		newState[i] = make([]uint8, size)
+		newState[i] = make([]string, size)
 	}
 
 	for i := 0; i < size; i++ {
 		for j := 0; j < size; j++ {
 			lifeAround := currentState.countLiving(size, i, j)
-			if currentState[i][j] == 1 {
+			if currentState[i][j] == "*" {
 				switch {
 				case lifeAround < 2:
-					newState[i][j] = 0
+					newState[i][j] = " "
 				case lifeAround == 2 || lifeAround == 3:
-					newState[i][j] = 1
+					newState[i][j] = "*"
 				default:
-					newState[i][j] = 0
+					newState[i][j] = " "
 				}
 			} else {
 				if lifeAround == 3 {
-					newState[i][j] = 1
+					newState[i][j] = "*"
+				} else {
+					newState[i][j] = " "
 				}
 			}
 		}
@@ -41,12 +43,12 @@ func (currentState State) countLiving(size, x, y int) uint8 {
 
 	for i := -1; i < 2; i++ {
 		for j := -1; j < 2; j++ {
-			if (x+i) == 0 && (y+j) == 0 {
+			if (x+i) == x && (y+j) == y {
 				continue
 			}
 			//check if the neighbour is in the square
 			if 0 <= (x+i) && (x+i) < size && 0 <= (y+j) && (y+j) < size {
-				if currentState[x+i][y+j] == 1 {
+				if currentState[x+i][y+j] == "*" {
 					totalLiving++
 				}
 			}
@@ -54,4 +56,38 @@ func (currentState State) countLiving(size, x, y int) uint8 {
 	}
 
 	return totalLiving
+}
+
+
+//BlockState initilize a still life block example
+func BlockState(size int) (State){
+	
+	Block := State(make([][]string, size))
+	
+	for i:=0; i< size; i++{
+		Block[i] = make([]string, size)
+	}
+
+	//init stable status
+	Block[0][0] = " "
+	Block[0][1] = " "
+	Block[0][2] = " "
+	Block[0][3] = " "
+
+	Block[1][0] = " "
+	Block[1][1] = "*"
+	Block[1][2] = "*"
+	Block[1][3] = " "
+
+	Block[2][0] = " "
+	Block[2][1] = "*"
+	Block[2][2] = "*"
+	Block[2][3] = " "
+
+	Block[3][0] = " "
+	Block[3][1] = " "
+	Block[3][2] = " "
+	Block[3][3] = " "
+
+	return Block
 }
